12fd164a82847302c7b7a203e224b05705d7a761
dsharma22 <sharmadeepshika22@gmail.com>
2021-08-24 16:55:52 +1200

    project initiation

---
+++ src/Student.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Student extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Students</h1>
+      </div>
+    );
+  }
+}
+
+export default Student;
